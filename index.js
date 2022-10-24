05c937ba17d1d5fbb513fa264709a7e9bedae27e
ChelseaAlejan <lexahdz05@gmail.com>
2022-10-24 15:50:05 -0500

    adjusted port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const html=require("./routes/html")
 const api=require('./routes/api.js')
-const PORT = 3001;
+const PORT = process.env.PORT ||3001;
 
 const app = express();
 
